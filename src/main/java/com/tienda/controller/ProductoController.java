2a3b7e87e3c150cb7666caa452bce96a0d00123f
CristoferLara <titolara975@gmail.com>
2023-11-05 21:51:05 -0600

    Proyecto Lunes Actualizado

--- src/main/java/com/tienda/controller/ProductoController.java
+++ src/main/java/com/tienda/controller/ProductoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-public class ProductoController {   
+public class ProductoController {    
 
     @Autowired
     private ProductoService productoService;
